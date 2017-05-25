5a3492e2b7b704afa2fade0424d520adb39ba966
Michael Siegfried <michael@procore.com>
2017-05-24 21:18:13 -0700

    Add initial specs.

---
+++ gatherer/spec/models/task_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Task do
+  let(:task) { Task.new }
+
+  it 'can distinguish a completed task' do
+    expect(task).not_to be_complete
+    task.mark_completed
+    expect(task).to be_complete
+  end
+end
