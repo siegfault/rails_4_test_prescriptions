aaac5babadbaf5583faab0ae0114bcf847caf597
Michael Siegfried <michael@procore.com>
2017-05-24 22:32:46 -0700

    Add ability to project schedule.

--- gatherer/app/models/project.rb
+++ gatherer/app/models/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :tasks
+  attr_accessor :due_date, :tasks
 
   def initialize
     @tasks = []
@@ -17,8 +17,28 @@ class Project
     incomplete_tasks.sum(&:size)
   end
 
+  def completed_velocity
+    tasks.sum(&:points_towards_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    projected_completion_date <= due_date
+  end
+
   private
   def incomplete_tasks
     tasks.reject(&:complete?)
+  end
+
+  def projected_completion_date
+    projected_days_remaining.days.from_now
   end
 end
--- gatherer/spec/models/project_spec.rb
+++ gatherer/spec/models/project_spec.rb
@@ -34,4 +34,32 @@ RSpec.describe Project do
       expect(project.remaining_size).to eq(5)
     end
   end
+
+  describe 'time data' do
+    let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) }
+    let(:old_done) { Task.new(size: 2, completed_at: 6.months.ago) }
+    let(:small_not_done) { Task.new(size: 1) }
+    let(:large_not_done) { Task.new(size: 4) }
+
+    before { project.tasks = [newly_done, old_done, small_not_done, large_not_done] }
+
+    it 'knows its velocity' do
+      expect(project.completed_velocity).to eq(3)
+    end
+
+    it 'knows its rate' do
+      expect(project.current_rate).to eq(1.0/7)
+    end
+
+    it 'knows its projected time remaining' do
+      expect(project.projected_days_remaining).to eq(35)
+    end
+
+    it 'knows if it is on schedule' do
+      project.due_date = 1.week.from_now
+      expect(project).not_to be_on_schedule
+      project.due_date = 6.months.from_now
+      expect(project).to be_on_schedule
+    end
+  end
 end
